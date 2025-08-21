eb735c2583fd00d887d8dda06b43ef919b215938
DasNerdwork <dasnerdwork@gmail.com>
2025-08-21 03:11:46 +0200

    fix: restored positioning

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -7,7 +7,7 @@ interface IntroProps {
 export const Intro = ({ hasSearched }: IntroProps) => {
   const { t } = useTranslation();
   return (
-    <div className={`text-center mb-6 transition-opacity duration-300 ease-in-out ${hasSearched ? "opacity-0 h-0" : "opacity-100"}`}>
+    <div className={`text-center mb-6 transition-opacity duration-300 ease-in-out translate-y-[25vh] ${hasSearched ? "opacity-0 h-0" : "opacity-100"}`}>
       <h2 className="text-3xl md:text-4xl font-extrabold mb-4 text-[var(--color-text)]">
         {t("introTitle")}
       </h2>
